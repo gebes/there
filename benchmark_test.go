38bd75825f759f207e5876b816cbf6d20a21e2db
wellisrite <yumedream12@gmail.com>
2022-07-03 16:15:50 +0700

    Remove . import

--- benchmark_test.go
+++ benchmark_test.go
@@ -3,24 +3,25 @@ package there_test
 /* Useful for testing. Commented for dependency cleanup
 import (
 	"encoding/json"
-	. "github.com/Gebes/there/v2"
-	"github.com/gin-gonic/gin"
-	"github.com/gorilla/mux"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/Gebes/there/v2"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 )
 
 func BenchmarkThere(b *testing.B) {
-	router := NewRouter()
-	router.Get("/", func(request Request) Response {
-		return Json(StatusOK, map[string]interface{}{
+	router := there.NewRouter()
+	router.Get("/", func(request there.Request) there.Response {
+		return there.Json(there.StatusOK, map[string]interface{}{
 			"text": "Hi",
 		})
 	})
 	for i := 0; i < b.N; i++ {
-		request := httptest.NewRequest(MethodGet, "/", nil)
+		request := httptest.NewRequest(there.MethodGet, "/", nil)
 		recorder := httptest.NewRecorder()
 
 		router.ServeHTTP(recorder, request)
@@ -38,7 +39,7 @@ func BenchmarkThere(b *testing.B) {
 func BenchmarkMux(b *testing.B) {
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Headers().Set(ResponseHeaderContentType, ContentTypeApplicationJson)
+		writer.Headers().Set(there.ResponseHeaderContentType, there.ContentTypeApplicationJson)
 		writer.WriteHeader(200)
 		data, err := json.Marshal(map[string]interface{}{
 			"text": "Hi",
@@ -49,7 +50,7 @@ func BenchmarkMux(b *testing.B) {
 		writer.Write(data)
 	})
 	for i := 0; i < b.N; i++ {
-		request := httptest.NewRequest(MethodGet, "/", nil)
+		request := httptest.NewRequest(there.MethodGet, "/", nil)
 		recorder := httptest.NewRecorder()
 
 		r.ServeHTTP(recorder, request)
@@ -74,7 +75,7 @@ func BenchmarkGin(b *testing.B) {
 	})
 
 	for i := 0; i < b.N; i++ {
-		request := httptest.NewRequest(MethodGet, "/", nil)
+		request := httptest.NewRequest(there.MethodGet, "/", nil)
 		recorder := httptest.NewRecorder()
 
 		r.ServeHTTP(recorder, request)
@@ -88,4 +89,4 @@ func BenchmarkGin(b *testing.B) {
 		result.Body.Close()
 	}
 }
-*/
+/*
