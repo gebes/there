0ff174a61ade61a50af7d6019b7bcef1e443d265
Christoph Krassnigg <christoph.krassnigg@icloud.com>
2022-11-30 17:49:27 +0100

    📝 Adapted test to new package

--- middlewares/skip_test.go
+++ middlewares/skip_test.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"errors"
+	"github.com/Gebes/there/v2/status"
 	"net/http/httptest"
 	"reflect"
 	"testing"
@@ -16,13 +17,13 @@ func TestSkip(t *testing.T) {
 
 	router.Get("/", dummyEndpointHandler)
 
-	request := httptest.NewRequest(there.MethodGet, "/", nil)
+	request := httptest.NewRequest(string(there.MethodGet), "/", nil)
 	recorder := httptest.NewRecorder()
 
 	router.ServeHTTP(recorder, request)
 	result := recorder.Result()
 
-	reflect.DeepEqual(result.StatusCode, there.StatusInternalServerError)
+	reflect.DeepEqual(result.StatusCode, status.InternalServerError)
 }
 
 func TestSkipFalse(t *testing.T) {
@@ -32,7 +33,7 @@ func TestSkipFalse(t *testing.T) {
 
 	router.Get("/", dummyEndpointHandler)
 
-	request := httptest.NewRequest(there.MethodGet, "/", nil)
+	request := httptest.NewRequest(string(there.MethodGet), "/", nil)
 	recorder := httptest.NewRecorder()
 
 	router.ServeHTTP(recorder, request)
@@ -48,25 +49,25 @@ func TestSkipNil(t *testing.T) {
 
 	router.Get("/", dummyEndpointHandler)
 
-	request := httptest.NewRequest(there.MethodGet, "/", nil)
+	request := httptest.NewRequest(string(there.MethodGet), "/", nil)
 	recorder := httptest.NewRecorder()
 
 	router.ServeHTTP(recorder, request)
 	result := recorder.Result()
 
-	reflect.DeepEqual(result.StatusCode, there.StatusInternalServerError)
+	reflect.DeepEqual(result.StatusCode, status.InternalServerError)
 }
 
-var dummyData  = map[string]interface{}{
+var dummyData = map[string]interface{}{
 	"Hello": "There",
 }
 
 func dummyMiddleware(request there.Request, next there.Response) there.Response {
-	return there.Error(there.StatusInternalServerError, errors.New("i'm just a dummy"))
+	return there.Error(status.InternalServerError, errors.New("i'm just a dummy"))
 }
 
 func dummyEndpointHandler(request there.Request) there.Response {
-	return there.Json(there.StatusOK, dummyData)
+	return there.Json(status.OK, dummyData)
 }
 
 func dummyValidatorFalse(request there.Request) bool {
